3ff54e1ceb16e3003787f3d552a079b7e37e8559
DavidePeron <davideperon94@gmail.com>
2018-03-26 21:59:24 +0200

    Completed 4.d, missing figure 10.c and d, bootstrap on PI to check

--- HW1/es4.m
+++ HW1/es4.m
@@ -2,7 +2,7 @@ clear all;
 close all;
 
 % Initial number of random variables
-n=10;
+n=100;
 % Max number of rvs is n*num_samples
 num_samples = 100;
 % Confidence level
@@ -14,6 +14,10 @@ mu = zeros(1,num_samples);
 std_dev = zeros(1,num_samples);
 ci_low = zeros(1,num_samples);
 ci_high = zeros(1,num_samples);
+pi_low_th = zeros(1,num_samples);
+pi_high_th = zeros(1,num_samples);
+pi_low_boot = zeros(1,num_samples);
+pi_high_boot = zeros(1,num_samples);
 zeta = zeros(1,num_samples);
 xi = zeros(1,num_samples);
 
@@ -23,27 +27,36 @@ for i=1:num_samples
 end
 
 for i=1:num_samples
-        % Generate n*i iid U(0,1) r.v.’s
-        rv = rand(1,n*i);
+    % Generate n*i iid U(0,1) r.v.’s
+    rv = rand(1,n*i);
 
-        % Find sample mean, sample std dev and 95%-confidence interval for the variance
-        mu(i) = mean(rv);
-        deviation = arrayfun(@(x) (x-mu(i))^2, rv);
-        std_dev(i) = sum(deviation)/(n*i-1);
-        %Use of bootstrap to calculate the CI for the variance
-        bootstrap = zeros(1,n*i);
-        ci = zeros(1,R);
-        for r = 1:R
-            for j = 1:n*i
-                pick = ceil(rand()*n*i);
-                bootstrap(j) = rv(pick);
-            end
-            deviation = arrayfun(@(x) (x-mu(i))^2, bootstrap);
-            ci(r) = sum(deviation)/(n*i-1);
+    % Find sample mean, sample std dev and 95%-confidence interval for the variance
+    mu(i) = mean(rv);
+		std_dev(i) = sum((rv-mu(i)).^2)/(n*i-1);
+    %Use of bootstrap to calculate the CI for the variance and PI
+    bootstrap = zeros(1,n*i);
+    ci = zeros(1,R);
+    pi = zeros(1,R);
+    for r = 1:R
+        for j = 1:n*i
+            pick = ceil(rand()*n*i);
+            bootstrap(j) = rv(pick);
         end
-        ci = sort(ci);
-        ci_low(i) = ci(r_0);
-        ci_high(i) = ci(R+1-r_0);
+        pi(r) = mean(bootstrap);
+        ci(r) = sum((bootstrap-mu(i)).^2)/(n*i-1);
+    end
+    ci = sort(ci);
+    ci_low(i) = ci(r_0);
+    ci_high(i) = ci(R+1-r_0);
+    pi = sort(pi);
+    pi_low_boot(i) = pi(r_0);
+    pi_high_boot(i) = pi(R+1-r_0);
+    
+    %Compute PI using theory (non normal distribution)
+    rv_sorted = sort(rv);
+    pi_low_th(i) = rv_sorted(floor(n*i*(1-gamma)/2)+1);
+    pi_high_th(i) = rv_sorted(ceil(n*i*(1+gamma)/2)+1);
+    
 end
 
 %Study the accuracy of the estimate with respect to the true value vs. n
@@ -67,15 +80,18 @@ ylabel('Squared error from the true value');
 title('Accuracy of the variance of rvs');
 
 figure('Name', 'Experiment4 - Confidence Intervals');
-plot(t,ci_high, '-.r');
+errorbar(t, std_dev, std_dev-ci_low, ci_high-std_dev, '.');
 grid on;
 title('Confidence Intervals for the Variance using Uniform Distribution U(0,1)');
-hold on;
-plot(t,ci_low, '-.m');
 xlabel('# of random variables');
 ylabel('Confidence Intervals');
-X = [t, fliplr(t)];
-Y = [ci_high, fliplr(ci_low)];
-fill(X, Y, 'y');
-hold on;
-plot(t,std_dev, '-b');
+
+figure('Name', 'Experiment4 - Prediction Intervals using theory');
+errorbar(t, mu, mu - pi_low_th, pi_high_th - mu, '.');
+grid on;
+title('Prediction intervals at level 0.95 using theory');
+
+figure('Name', 'Experiment4 - Prediction Interval using bootstrap');
+errorbar(t, mu, mu - pi_low_boot, pi_high_boot - mu, '.');
+grid on;
+title('Prediction intervals at level 0.95 using bootstrap');
--- HW1/es4_N.m
+++ HW1/es4_N.m
@@ -2,20 +2,24 @@ clear all;
 close all;
 
 % Initial number of random variables
-n=10;
+n=100;
 % Max number of rvs is n*num_samples
 num_samples = 100;
 % #experiments for each sample
 num_exp = 100;
 % Confidence level
 gamma = 0.95;
+r_0 = 25;
+R = ceil(2*r_0/(1-gamma)) - 1;
 
-mu = zeros(num_exp,num_samples);
-std_dev = zeros(num_exp,num_samples);
-ci_low = zeros(num_exp,num_samples);
-ci_high = zeros(num_exp,num_samples);
-pi_low = zeros(num_exp,num_samples);
-pi_high = zeros(num_exp,num_samples);
+mu = zeros(1,num_samples);
+std_dev = zeros(1,num_samples);
+ci_low = zeros(1,num_samples);
+ci_high = zeros(1,num_samples);
+pi_low = zeros(1,num_samples);
+pi_high = zeros(1,num_samples);
+pi_low_boot = zeros(1,num_samples);
+pi_high_boot = zeros(1,num_samples);
 zeta = zeros(1,num_samples);
 xi = zeros(1,num_samples);
 
@@ -27,31 +31,41 @@ end
 % disp(zeta);
 
 for i=1:num_samples
-	for j= 1:num_exp
 		% Generate n=48 iid U(0,1) r.v.’s
 		rv = normrnd(0,1,[1 n*i]);
 
 		% Find sample mean, sample std dev and 95%-confidence interval for the variance
-		mu(j,i) = mean(rv);
-		deviation = arrayfun(@(x) (x-mu(j,i))^2, rv);
-		std_dev(j,i) = sum(deviation)/(n*i-1);
-		ci_low(j,i) = sqrt(std_dev(j,i)*zeta(i)/(n*i-1));
-		ci_high(j,i) = sqrt(std_dev(j,i)*xi(i)/(n*i-1));
-    pi_low(j,i) = mu(j,i) - 1.99*sqrt(std_dev(j,i));
-    pi_high(j,i) = mu(j,i) + 1.99*sqrt(std_dev(j,i));
-	end
+		mu(i) = mean(rv);
+		std_dev(i) = sum((rv-mu(i)).^2)/(n*i-1);
+		ci_low(i) = sqrt(std_dev(i)*zeta(i)/(n*i-1));
+		ci_high(i) = sqrt(std_dev(i)*xi(i)/(n*i-1));
+    pi_low(i) = mu(i) - 1.99*sqrt(std_dev(i));
+    pi_high(i) = mu(i) + 1.99*sqrt(std_dev(i));
+    
+    bootstrap = zeros(1,n*i);
+    pi = zeros(1,R);
+    for r = 1:R
+        for j = 1:n*i
+            pick = ceil(rand()*n*i);
+            bootstrap(j) = rv(pick);
+        end
+        pi(r) = mean(bootstrap);
+    end
+    pi = sort(pi);
+    pi_low_boot(i) = pi(r_0);
+    pi_high_boot(i) = pi(R+1-r_0);
 end
 
-mean_mu = mean(mu,1);
-mean_std_dev = mean(std_dev,1);
-mean_ci_low = mean(ci_low,1);
-mean_ci_high = mean(ci_high,1);
-mean_pi_low = mean(pi_low,1);
-mean_pi_high = mean(pi_high,1);
+% $$$ mean_mu = mean(mu,1);
+% $$$ mean_std_dev = mean(std_dev,1);
+% $$$ mean_ci_low = mean(ci_low,1);
+% $$$ mean_ci_high = mean(ci_high,1);
+% $$$ mean_pi_low = mean(pi_low,1);
+% $$$ mean_pi_high = mean(pi_high,1);
 
 %Study the accuracy of the estimate with respect to the true value vs. n
-mean_err_from_true = abs(mean_mu);
-std_dev_err_from_true = abs(mean_std_dev - 1);
+mean_err_from_true = abs(mu);
+std_dev_err_from_true = abs(std_dev - 1);
 
 %Plot the results
 t = linspace(n, n*num_samples, num_samples);
@@ -70,20 +84,25 @@ ylabel('Squared error from the true value');
 title('Accuracy of the variance of rvs');
 
 figure('Name', 'Experiment4 - Confidence Intervals');
-plot(t,mean_ci_high, '-.r');
+errorbar(t,std_dev, std_dev - ci_low, ci_high - std_dev, '-');
 grid on;
 title('Confidence Intervals for the Variance using Normal Distribution N(0,1)');
-hold on;
-plot(t,mean_ci_low, '-.m');
+% $$$ hold on;
+% $$$ plot(t,ci_low, '-.m');
 xlabel('# of random variables');
 ylabel('Confidence Intervals');
-X = [t, fliplr(t)];
-Y = [mean_ci_high, fliplr(mean_ci_low)];
-fill(X, Y, 'y');
-hold on;
-plot(t,mean_std_dev, '-b');
+% $$$ X = [t, fliplr(t)];
+% $$$ Y = [ci_high, fliplr(ci_low)];
+% $$$ fill(X, Y, 'y');
+% $$$ hold on;
+% $$$ plot(t,std_dev, '-b');
 
-figure('Name', 'Experiment4 - Prediction Intervals');
-errorbar(t, mean_mu, mean_mu - mean_pi_low, mean_pi_high - mean_mu, '.');
+figure('Name', 'Experiment4 - Prediction Intervals using theory');
+errorbar(t, mu, mu - pi_low, pi_high - mu, '.');
 grid on;
 title('Prediction intervals at level 0.95 using theory');
+
+figure('Name', 'Experiment4 - Prediction Interval using bootstrap');
+errorbar(t, mu, mu - pi_low_boot, pi_high_boot - mu, '.');
+grid on;
+title('Prediction intervals at level 0.95 using bootstrap');
